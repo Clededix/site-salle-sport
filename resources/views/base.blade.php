ac7b2d3726a3257b8cbc7877ed69b0862fa873d0
[clededix] <[chauvir@pm.me]>
2024-11-03 15:38:31 +0100

    FAIL TO SOLVE CSS issue

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('resources/css/app.css') }}">
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
