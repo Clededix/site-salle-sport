6ad1cb7b023a7ef311abf0d6ea0a0592b6cd8b17
[clededix] <[chauvir@pm.me]>
2024-11-03 12:59:11 +0100

    ADDING routes web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AccueilController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\CoursController;
+use App\Http\Controllers\MentionsLegalesController;
+use App\Http\Controllers\TarifController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [AccueilController::class, 'index'])->name('accueil');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::get('/cours', [CoursController::class, 'index'])->name('cours');
+Route::get('/mentionsLegales', [MentionsLegalesController::class, 'index'])->name('MentionsLegales');
+Route::get('/tarif', [TarifController::class, 'index'])->name('tarif');
+
